6f93ccea41d4ddf8311570db5800b6edb07929b2
lpalominosf <lorena.palominos@gmail.com>
2017-12-13 12:13:44 -0300

    añadiendo fontawesome y algunos avances

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	$('#status').delay(5000).fadeOut('slow');
-	$('#preloader').delay(5000).fadeOut();
+	$('#status').delay(3000).fadeOut('slow');
+	$('#preloader').delay(3000).fadeOut();
 	$('.second-screen').hide();
 
 
 
 	setTimeout(function(){
 	$('.second-screen').show();
-	}, 5000);
+	}, 3000);
 
 });
