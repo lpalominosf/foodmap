bc14a833e0421325d7fda97e06b09c2847f00f8d
lpalominosf <lorena.palominos@gmail.com>
2017-12-13 12:43:22 -0300

    añadiendo segunda pantalla

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	$('#status').delay(3000).fadeOut('slow');
-	$('#preloader').delay(3000).fadeOut();
+	$('#status').delay(1000).fadeOut('slow');
+	$('#preloader').delay(1000).fadeOut();
 	$('.second-screen').hide();
 
 
 
 	setTimeout(function(){
 	$('.second-screen').show();
-	}, 3000);
+	}, 1000);
 
 });
