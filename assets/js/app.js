46a533cad562eb1c673bdfb79328ec81ce94cf99
lpalominosf <lorena.palominos@gmail.com>
2017-12-14 09:29:06 -0300

    añadiendo correcciones y nuevo commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,69 @@
+/*
+*Función que recarga toda la página
+*/
+
 $(document).ready(function(){
+  /*
+  *Columna donde va el titulo del preloader y tiempo que durará la animación
+  */
 	$('#status').delay(4000).fadeOut('slow');
 	$('#preloader').delay(4000).fadeOut();
 	$('.second-screen').hide();
 
 
 /*
-*https://www.w3schools.com/howto/howto_js_portfolio_filter.asp /// Esto usé para poder realizar el filtro de imágenes
+*Función que permite que la animación principal termine y se pase a la segunda pantalla
 */
   setTimeout(function() {
   $('.second-screen').show();
   }, 4000);
-
-
-
-  filterSelection("all") // Execute the function and show all columns
-  function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("column");
-    if (c == "all") c = "";
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-      assetsRemoveClass(x[i], "show");
-      if (x[i].className.indexOf(c) > -1) assetsAddClass(x[i], "show");
-    }
-  }
-
-  // Show filtered elements
-  function assetsAddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
-    }
-  }
-
-  // Hide elements that are not selected
-  function assetsRemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-
-  // Add active class to the current button (highlight it)
-  var btnContainer = document.getElementById("myBtnContainer");
-  var btns = btnContainer.getElementsByClassName("btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
 });
+
+
+/*
+*https://www.w3schools.com/howto/howto_js_portfolio_filter.asp /// Esto usé para poder realizar el filtro de imágenes
+*/
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+  // Muestra los elementos filtrados
+  function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+  // Esconde elementos que no han sido seleccionados
+  function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Agrega una clase activa al boton seleccionado
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
