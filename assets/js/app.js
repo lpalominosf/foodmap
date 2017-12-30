a7b2631b3180edd8bb5f86d8f8162dfd51c0e5f0
Lore Palominos <lorena.palominos@gmail.com>
2017-12-30 16:49:45 -0300

    añadiendo bootstrap 3 y modal de cada tipo de comida

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,7 +34,9 @@ function filterSelection(c) {
   }
 }
 
-  // Muestra los elementos filtrados
+/*
+*Muestra los elementos filtrados
+*/
   function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -44,7 +46,9 @@ function filterSelection(c) {
   }
 }
 
-  // Esconde elementos que no han sido seleccionados
+/*
+*Esconde elementos que no han sido seleccionados
+*/
   function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -57,7 +61,9 @@ function filterSelection(c) {
   element.className = arr1.join(" ");
 }
 
-// Agrega una clase activa al boton seleccionado
+/*
+*Agrega una clase activa al boton seleccionado
+*/
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -66,4 +72,97 @@ for (var i = 0; i < btns.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
+
+
+/*
+*Esta función llama a cada comida y muestra un mapa con la ubicación del restaurante del tipo de comida recomendado
+*/
+$("#chinese-btn").click(function(){
+    $("#food1").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Hao Hwa</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d13317.629073157572!2d-70.63952465!3d-33.438694399999996!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2scl!4v1514586598620' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Monjitas 386, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+    $("#food2").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Hao Hwa</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d13317.629073157572!2d-70.63952465!3d-33.438694399999996!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2scl!4v1514586598620' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Monjitas 386, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+        $("#food3").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Hao Hwa</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d13317.629073157572!2d-70.63952465!3d-33.438694399999996!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2scl!4v1514586598620' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Monjitas 386, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+});
+/*
+*Función que cierra el modal
+*/
+$('#close').click(function() {
+    $(this).parent().hide();
+    $(".modal-content").empty();
+  });
+
+$("#japanese-btn").click(function(){
+    $("#food4").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Kintaro</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.4840805393874!2d-70.64626804967304!3d-33.43669258068359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5985fd62801%3A0x431e8ad43a3eb3ae!2sRamen+Kintaro!5e0!3m2!1ses!2scl!4v1514661516539' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Monjitas 460, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+    $("#food5").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Kintaro</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.4840805393874!2d-70.64626804967304!3d-33.43669258068359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5985fd62801%3A0x431e8ad43a3eb3ae!2sRamen+Kintaro!5e0!3m2!1ses!2scl!4v1514661516539' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Monjitas 460, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+    $("#food6").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Kintaro</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.4840805393874!2d-70.64626804967304!3d-33.43669258068359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5985fd62801%3A0x431e8ad43a3eb3ae!2sRamen+Kintaro!5e0!3m2!1ses!2scl!4v1514661516539' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Monjitas 460, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+});
+
+$("#thai-btn").click(function(){
+    $("#food7").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Thai-Isan</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d106553.41908741753!2d-70.7721848!3d-33.4286056!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c4533d095555%3A0x11be086793f8a152!2sThai-Isan!5e0!3m2!1ses!2scl!4v1514663026537' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Huérfanos 2460, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+    $("#food8").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Thai-Isan</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d106553.41908741753!2d-70.7721848!3d-33.4286056!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c4533d095555%3A0x11be086793f8a152!2sThai-Isan!5e0!3m2!1ses!2scl!4v1514663026537' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Huérfanos 2460, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+    $("#food9").click(function(){
+    $("#modal1").show();
+    $(".modal-content").append("<span class='glyphicon glyphicon-remove' aria-hidden='true' id='close'></span>"+
+                              "<div class='row'><h1 id='titlemodal' class='text-center'>Thai-Isan</h1></div>"+
+                              "<div class='row' id='mapmodal'><iframe class='col-xs-12 text-center' src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d106553.41908741753!2d-70.7721848!3d-33.4286056!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c4533d095555%3A0x11be086793f8a152!2sThai-Isan!5e0!3m2!1ses!2scl!4v1514663026537' width='300' height='200' frameborder='0' style='border:0' allowfullscreen></iframe>"+
+                              "<div class='row'><p class='text-center col-xs-10 col-xs-offset-1' id='addressmodal'>Huérfanos 2460, Santiago, Región Metropolitana</p></div>" +
+                              "<div class='row'><button id='btnmodal' class='btn btn-pedir btn-default col-xs-4 col-xs-offset-4'>Pedir</button></div")
+});
+});
+
 }
